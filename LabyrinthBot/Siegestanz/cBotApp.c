23eeadb3b9a741ec94ef769d9177e00ccd574777
Tobias <tobiasknell@web.de>
2023-04-10 15:13:06 +0200

    Add Victory Dance starter file

---
+++ LabyrinthBot/Siegestanz/cBotApp.c
@@ -0,0 +1,28 @@
+// Dieses Program führt den Siegestanz aus
+
+#include <cBotApp.h>
+
+#include <stdio.h>
+
+// Precondition: Labyrinth erfolgreich gelöst -> Am Zielfeld angekommen
+
+void init(){
+	// Im Feld mittig ausrichten -> Erstmal auslassen
+	// Displayausgabe - Sieg
+	// Start Song
+}
+
+
+
+void loop(){
+
+	// Hörmann Diskobot Abbruchbedingung wenn Song vorbei
+		//Play Again Displayanzeige
+		// (Softe LED Lichter)
+
+	// LED Lichter wechselnd blinken
+
+	// (Random) Rechts und links im Kreis drehen lassen
+
+
+}
