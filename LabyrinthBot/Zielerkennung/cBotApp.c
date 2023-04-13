a6bd5574e4ed2d14ca26f8de1f3c762024dd3037
Tobias <tobiasknell@web.de>
2023-04-13 20:22:54 +0200

    Add initial file for finding goalfield

---
+++ LabyrinthBot/Zielerkennung/cBotApp.c
@@ -0,0 +1,31 @@
+/*
+ * Zielerkennung
+ * Dieses Programm zeigt, wie die Zielerkennung mit Hilfe einer Marke auf dem Boden
+ * und den Lichtsensoren erreicht werden kann.
+ *
+ * In ersten Tests wird das Programm genutzt, um zu sehen, wie die Sensoren
+ * reagieren, wenn über verschiedene Muster gefahren wird.
+ *
+ * Nach den Tests wird ein Muster ausgewählt, das als "Zielmarke" genutzt wird.
+ *
+ * Anschließend wird eine Funktion entwickelt, die dieses Muster zuverlässig
+ * erkennt und in die anderen Programme integriert werden kann.
+ */
+
+
+#include "cBotApp.h"
+#include <stdlib.h>
+
+void init(){
+
+}
+
+void loop(){
+
+	// Erste Tests
+	// Roboter auf Knopfdruck gerade nach vorne fahren & stoppen lassen
+
+	// Lichtsensor abfragen
+
+	// Daten von Lichtsensor auf Display ausgeben
+}
